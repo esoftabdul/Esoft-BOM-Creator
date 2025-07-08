95e38d2badd1177dde2756659c804f91b3daf9d2
esoftabdul <abdul.shaikh@esoftech.com>
2025-07-08 09:22:38 +0000

    feat :custom doctypes added for bom creator

---
+++ esoft_bom_creator/esoft_bom_creator/doctype/operation_charges_master/test_operation_charges_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Abdul and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOperationChargesMaster(FrappeTestCase):
+	pass
